4d7e8053c541c731dbbf794c23fa89c99a358749
huengo308 <1851061355@e.tlu.edu.vn>
2021-01-11 20:49:07 +0700

    add file btl

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+        <div class="row">
+            <p>
+                Copyright &copy; 2020 by Ngo Thi Hue. All rights reseved
+            </p>
+        </div>
+      </footer>
---
+++ header.php
@@ -0,0 +1,33 @@
+
+  <header>
+      <nav>
+      <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tài khoản</a>
+      <div class="dropdown-menu" aria-labelledby="dropdownId">
+        <a class="dropdown-item" href="#">Tài khoản</a>
+        <a class="dropdown-item" href="login.php">Đăng nhập</a>
+        <a class="dropdown-item" href="register.php">Đăng ký</a>
+        <a class="dropdown-item" href="logout.php">Đăng xuất</a>
+      </div>
+        <ul class="nav-links">
+          <li>
+            <a href="index.php"><i class="fas fa-home"></i>Home</a>
+          </li>
+          <li>
+            <a href="about.php"><i class="fas fa-camera-retro"></i>About</a>
+          </li>
+          <li>
+            <a href="resume.php"><i class="far fa-address-card"></i>Resume</a>
+          </li>
+          <li>
+            <a href="work.php"><i class="fas fa-briefcase"></i>works</a>
+          </li>
+          <li>
+            <a href="blog.php"><i class="fas fa-blog"></i>Blog</a>
+          </li>
+        </ul>
+         <div class="burger">
+          <i class="fas fa-bars"></i>
+         </div>
+      </nav>
+    </header>
+    
